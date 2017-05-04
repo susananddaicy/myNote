b8dab4cbde3f49bcccfb3c7e572eeb0af7609cc5
daichunyu747 <daichunyu747@lu.com>
2017-05-04 13:53:35 +0800

    添加模块

--- proj/dist/main.js
+++ proj/dist/main.js
@@ -1,3 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeter_1 = require("./greeter");
+console.log(greeter_1.sayHello("TypeScript"));
 function hello(compiler) {
     console.log("Hello from " + compiler);
 }
--- proj/src/main.ts
+++ proj/src/main.ts
@@ -1,3 +1,8 @@
+import { sayHello } from "./greeter";
+
+console.log(sayHello("TypeScript"));
+
+
 function hello(compiler: string) {
     console.log(`Hello from ${compiler}`);
 }
